0b76a21c28bcbad856549a4b5627f545b9e0e454
SithScripter <forwardtoanuj@gmail.com>
2025-07-19 17:42:49 +0530

    Refactor

--- vars/startDockerGrid.groovy
+++ vars/startDockerGrid.groovy
@@ -1,9 +1,13 @@
 def call(String composeFile = 'docker-compose-grid.yml', int waitSeconds = 20) {
     try {
-        echo "🚀 Starting Docker-based Selenium Grid using ${composeFile}..."
-        sh "docker-compose -f ${composeFile} up -d"
+        echo "🚀 Starting Docker-based Selenium Grid using ${composeFile} with job context: ${env.JOB_NAME}..."
+        sh "docker-compose -p ${env.JOB_NAME} -f ${composeFile} up -d"
         echo "⏳ Waiting ${waitSeconds} seconds for Grid to stabilize..."
         sleep time: waitSeconds, unit: 'SECONDS'
+		
+		echo "✅ Grid status:"
+		sh "docker-compose -p ${env.JOB_NAME} -f ${composeFile} ps"
+		
     } catch (e) {
         error "❌ Failed to start Docker Grid: ${e.getMessage()}"
     }
--- vars/stopDockerGrid.groovy
+++ vars/stopDockerGrid.groovy
@@ -1,7 +1,7 @@
 def call(String composeFile = 'docker-compose-grid.yml') {
     try {
-        echo "🧹 Stopping Selenium Grid..."
-        sh "docker-compose -f ${composeFile} down || echo \"⚠️ Grid already stopped or not found.\""
+        echo "🧹 Stopping Docker-based Selenium Grid using job context: ${env.JOB_NAME}..."
+        sh "docker-compose -p ${env.JOB_NAME} -f ${composeFile} down || echo \"⚠️ Grid already stopped or not found.\""
     } catch (e) {
         echo "⚠️ Exception during grid shutdown: ${e.getMessage()}"
     }
