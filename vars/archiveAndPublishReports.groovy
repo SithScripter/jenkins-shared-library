d7a4f8b5e7b0a5d3efa1948ea8b18df9496050ab
SithScripter <forwardtoanuj@gmail.com>
2025-09-30 15:47:39 +0530

    Refactor

--- vars/archiveAndPublishReports.groovy
+++ vars/archiveAndPublishReports.groovy
@@ -3,18 +3,6 @@ def call() {
     archiveArtifacts artifacts: 'logs/**', allowEmptyArchive: true
 
     publishHTML([
-        reportName: 'Chrome Report',
-        reportDir: 'reports/chrome',
-        reportFiles: 'index.html',
-        keepAll: true, alwaysLinkToLastBuild: true, allowMissing: false
-    ])
-    publishHTML([
-        reportName: 'Firefox Report',
-        reportDir: 'reports/firefox',
-        reportFiles: 'index.html',
-        keepAll: true, alwaysLinkToLastBuild: true, allowMissing: false
-    ])
-    publishHTML([
         reportName: 'Cumulative Dashboard',
         reportDir: 'reports',
         reportFiles: 'index.html',
