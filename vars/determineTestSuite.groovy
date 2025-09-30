54203b8e4ec6d6202bc861b2e4d30c13c2187eb6
SithScripter <forwardtoanuj@gmail.com>
2025-09-30 16:29:38 +0530

    Refactor

---
+++ vars/determineTestSuite.groovy
@@ -0,0 +1,15 @@
+def call() {
+    def causes = currentBuild.getBuildCauses()
+    def descs = causes*.shortDescription.join(', ')
+    echo "🔍 Build was triggered by: ${descs}"
+  
+    def isTimerTrigger  = descs.toLowerCase().contains('timer') || descs.toLowerCase().contains('cron')
+    def isManualTrigger = descs.toLowerCase().contains('started by user')
+  
+    def suiteToRun = isTimerTrigger ? 'regression' : (params.SUITE_NAME ?: 'smoke')
+    
+    echo "✅ Pipeline will run the '${suiteToRun}' suite."
+    echo "DEBUG: params.SUITE_NAME='${params.SUITE_NAME}', suiteToRun='${suiteToRun}'"
+    
+    return suiteToRun
+}
